c486ea6c9e9ea69ca524c62ef2885df9aa46c6fb
Dmitriy Antipov <aston_bonny@mail.ru>
2022-04-08 20:22:33 +0300

    07_04_22_4

--- sketch_apr02a_walker/sketch_apr02a_walker.ino
+++ sketch_apr02a_walker/sketch_apr02a_walker.ino
@@ -11,8 +11,8 @@
 
 #define RESET_BTN_PIN 8 // Кнопка для мягкого перезапуска
 
-#define SERVO_MOT_L_PIN 2 // Пин левого серво мотора
-#define SERVO_MOT_R_PIN 4 // Пин правого серво мотора
+#define SERVO_MOT_L_PIN 9 // Пин левого серво мотора
+#define SERVO_MOT_R_PIN 10 // Пин правого серво мотора
 
 #define LEFT_LINE_SENSOR_PIN A0 // Пин левого датчика линии
 #define RIGHT_LINE_SENSOR_PIN A1 // Пин правого датчика линии
@@ -20,21 +20,21 @@
 #define SERVO_MOT_L_DIR_MODE 1 // Режим вращения левого мотора, где нормально 1, реверс -1
 #define SERVO_MOT_R_DIR_MODE -1 // Режим вращения правого мотора
 
-#define LEFT_RAW_REF_BLACK_LINE_SEN 756 // Значение чёрного левого датчика линии
-#define LEFT_RAW_REF_WHITE_LINE_SEN 48 // Значение белого левого датчика линии
+#define LEFT_RAW_REF_BLACK_LINE_SEN 531 // Значение чёрного левого датчика линии
+#define LEFT_RAW_REF_WHITE_LINE_SEN 34 // Значение белого левого датчика линии
 
-#define RIGHT_RAW_REF_BLACK_LINE_SEN 755 // Значение чёрного правого датчика линии
-#define RIGHT_RAW_REF_WHITE_LINE_SEN 45 // Значение белого правого датчика линии
+#define RIGHT_RAW_REF_BLACK_LINE_SEN 533 // Значение чёрного правого датчика линии
+#define RIGHT_RAW_REF_WHITE_LINE_SEN 33 // Значение белого правого датчика линии
 
 #define MIN_SPEED_FOR_SERVO_MOT 10 // Минимальное значение для старта серво мотора
 
 Servo lServoMot, rServoMot; // Инициализация объектов моторов
-GTimer myTimer(10); // Инициализация объекта таймера
+GTimer myTimer(MS, 10); // Инициализация объекта таймера
 GButton btn(RESET_BTN_PIN); // Инициализация кнопки
 
 unsigned long currTime, prevTime, loopTime; // Время
 
-float Kp = 0.5, Ki = 0, Kd = 0; // Коэффиценты регулятора при старте
+float Kp = 0.25, Ki = 0, Kd = 0; // Коэффиценты регулятора при старте
 
 GyverPID regulator(Kp, Ki, Kd, 10); // Инициализируем коэффициенты регулятора
 
@@ -43,21 +43,22 @@ void(* softResetFunc) (void) = 0; // Функция мягкого переза�
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(100);
-  //pinMode(RESET_BTN_PIN, INPUT_PULLUP); // Подключение кнопки Start/stop/reset
-  btn.setType(HIGH_PULL); // LOW_PULL/HIGH_PULL
+  Serial.println();
+  // Подключение кнопки start/stop/reset
+  btn.setDebounce(50); // Настройка антидребезга кнопки (по умолчанию 80 мс)
+  btn.setTimeout(300); // Настройка таймаута на удержание кнопки (по умолчанию 500 мс)
+  btn.setClickTimeout(600); // Настройка таймаута между кликами по кнопке (по умолчанию 300 мс)
+  btn.setType(HIGH_PULL); // HIGH_PULL - кнопка подключена к GND, пин подтянут к VCC, LOW_PULL  - кнопка подключена к VCC, пин подтянут к GND
   btn.setDirection(NORM_OPEN); // NORM_OPEN - нормально-разомкнутая кнопка, NORM_CLOSE - нормально-замкнутая кнопка
   btn.setTickMode(AUTO); // MANUAL - нужно вызывать функцию tick() вручную, AUTO - tick() входит во все остальные функции и опрашивается сама!
-  pinMode(LEFT_LINE_SENSOR_PIN, INPUT); // Настойка пина левого датчика линии
   pinMode(RIGHT_LINE_SENSOR_PIN, INPUT); // Настойка пина правого датчика линии
-  lServoMot.attach(SERVO_MOT_L_PIN); rServoMot.attach(SERVO_MOT_R_PIN); // Подключение моторов
+  // Моторы
+  lServoMot.attach(SERVO_MOT_L_PIN, 500, 2500); rServoMot.attach(SERVO_MOT_R_PIN, 500, 2500); // Подключение моторов
   MotorSpeed(lServoMot, 0, SERVO_MOT_L_DIR_MODE); MotorSpeed(rServoMot, 0, SERVO_MOT_R_DIR_MODE); // При старте моторы выключаем
   regulator.setDirection(NORMAL); // Направление регулирования (NORMAL/REVERSE)
   regulator.setLimits(-90, 90); // Пределы регулятора
-  Serial.println();
-  Serial.println("Ready... Pres btn");
-  while (true) {
-    if (btn.isSingle()) break;
-  }
+  Serial.println("Ready... Press btn");
+  while (!btn.isClick()); // Цикл, в котором проверяем, что нажали на кнопку
   Serial.println("Go!!!");
 }
 
@@ -65,8 +66,7 @@ void loop() {
   currTime = millis();
   loopTime = currTime - prevTime;
   prevTime = currTime;
-  //if (!digitalRead(RESET_BTN_PIN)) softResetFunc(); // Если клавиша нажата, то сделаем мягкую перезагрузку
-  //if (btn.isClick()) softResetFunc(); // Если клавиша нажата, то сделаем мягкую перезагрузку
+  if (btn.isClick()) softResetFunc(); // Если клавиша нажата, то сделаем мягкую перезагрузку
   if (myTimer.isReady()) { // Раз в 10 мсек выполнять
     // Считываем сырые значения с датчиков линии
     int lRawRefLineS = analogRead(LEFT_LINE_SENSOR_PIN);
@@ -79,11 +79,13 @@ void loop() {
     Serial.print("lLineS: "); Serial.print(lRefLineS); Serial.print("\t");
     Serial.print("rLineS: "); Serial.print(rRefLineS); Serial.println("\t");
     int error = lRefLineS - rRefLineS; // Нахождение ошибки
+    Serial.print("error: "); Serial.println(error);
     regulator.setpoint = error; // Передаём ошибку
-    regulator.setDt(loopTime); // Установка dt для регулятора
+    //regulator.setDt(loopTime); // Установка dt для регулятора
     float u = regulator.getResult(); // Управляющее воздействие с регулятора
-    MotorsControl(0, 90);
-    //MotorSpeed(lServoMot, 50 + u, SERVO_MOT_L_DIR_MODE); MotorSpeed(rServoMot, 50 - u, SERVO_MOT_R_DIR_MODE);
+    Serial.print("u: "); Serial.println(u);
+    MotorsControl(u, 30);
+    //MotorSpeed(lServoMot, 50, SERVO_MOT_L_DIR_MODE); MotorSpeed(rServoMot, 50, SERVO_MOT_R_DIR_MODE);
   }
 }
 
@@ -93,7 +95,7 @@ void MotorsControl(int dir, int speed) {
   float z = (float) speed / max(abs(lServoMotSpeed), abs(rServoMotSpeed)); // Вычисляем отношение желаемой мощности к наибольшей фактической
   lServoMotSpeed *= z, rServoMotSpeed *= z;
   lServoMotSpeed = constrain(lServoMotSpeed, -90, 90), rServoMotSpeed = constrain(rServoMotSpeed, -90, 90);
-  Serial.print(lServoMotSpeed); Serial.print(", "); Serial.println(rServoMotSpeed);
+  //Serial.print(lServoMotSpeed); Serial.print(", "); Serial.println(rServoMotSpeed);
   MotorSpeed(lServoMot, lServoMotSpeed, SERVO_MOT_L_DIR_MODE); MotorSpeed(rServoMot, rServoMotSpeed, SERVO_MOT_R_DIR_MODE);
 }
 
@@ -101,14 +103,14 @@ void MotorsControl(int dir, int speed) {
 void MotorSpeed(Servo servoMot, int speed, int rotateMode) {
   // Servo, 0->FW, 90->stop, 180->BW
   speed = constrain(speed, -90, 90) * rotateMode;
-  Serial.print("servoMotSpeed "); Serial.print(speed);
+  //Serial.print("servoMotSpeed "); Serial.print(speed);
   if (speed >= 0) {
     speed = map(speed, 0, 90, 90, 180);
   } else {
     speed = map(speed, 0, -90, 90, 0);
   }
   servoMot.write(speed);
-  Serial.print(" convertedMotSpeed "); Serial.println(speed);
+  //Serial.print(" convertedMotSpeed "); Serial.println(speed);
 }
 
 // Калибровка и нормализация значений с датчика линии
